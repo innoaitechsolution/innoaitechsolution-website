e5579aa4dc5ea67834a762de013f0e8219f557a6
Arzu Caner <arzuguneycaner@gmail.com>
2024-04-03 15:28:35 +0100

    Update navbar

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './skills.css';
-import UIDesign from '../../assets/ui-design.png';
-import WebDesign from '../../assets/website-design.png';
-import AppDesign from '../../assets/app-design.png';
+import WebDev from '../../assets/webdev.png';
+import MobileAppDev from '../../assets/mobileappdev.png';
+import DatabaseDev from '../../assets/databasedev.png';
 
 const Skills = () => {
     return (
@@ -11,23 +11,23 @@ const Skills = () => {
             <span className="skillDesc">Lorem.</span>
             <div className="skillBars">
                 <div className="skillBar">
-                    <img src={UIDesign} alt="UIDesign" className="skillBarImg" />
+                    <img src={WebDev} alt="WebDevelopent" className="skillBarImg" />
                     <div className="skillBarText">
-                        <h2>UI/UX Design</h2>
+                        <h2>Web Developent</h2>
                         <p>This is a demo text, you can write your own content here.</p>
                     </div>
                 </div>
                 <div className="skillBar">
-                    <img src={WebDesign} alt="WebDesign" className="skillBarImg" />
+                    <img src={MobileAppDev} alt="MobilAppDevelopent" className="skillBarImg" />
                     <div className="skillBarText">
-                        <h2>Website Design</h2>
+                        <h2>Mobile App Development</h2>
                         <p>This demo text can be changed while making the production ready website.</p>
                     </div>
                 </div>
                 <div className="skillBar">
-                    <img src={AppDesign} alt="AppDesign" className="skillBarImg" />
+                    <img src={DatabaseDev} alt="DatabaseDevelopment" className="skillBarImg" />
                     <div className="skillBarText">
-                        <h2>App Design</h2>
+                        <h2>Database Development</h2>
                         <p>You can write text related to mobile app development.</p>
                     </div>
                 </div>
