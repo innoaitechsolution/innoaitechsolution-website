e02a46c6fb990b751d24dd9520889d60edab73f4
Arzu Caner <arzuguneycaner@gmail.com>
2024-04-03 22:31:00 +0100

    update navbar

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -10,8 +10,8 @@ const Intro = () => {
             <div className="introContent">
                 <span className="hello">Hello,</span>
                 <span className="introText">We are <span className="introName">software</span> <br />Company</span>
-                <p className="introPara">lorem<br />lorem.</p>
-                <Link><button className="btn"><img src={btnImg} alt="Hire" className='btnImg'/> lorem</button></Link>
+                <p className="introPara">InnoaI Tech Solution<br />Web Design, AI, Database, Software Solutions</p>
+                <Link><button className="btn"><img src={btnImg} alt="Hire" className='btnImg'/>Empower Your Digital Presence with Us!</button></Link>
             </div>
             <img src={bg} alt="Profile" className="bg" />
         </section>
