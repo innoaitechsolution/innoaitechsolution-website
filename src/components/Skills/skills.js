6523217ca23e93af37a43a8bd99c351a7aa48419
Arzu Caner <arzuguneycaner@gmail.com>
2024-04-10 23:15:49 +0100

    Fix the skills

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -8,13 +8,14 @@ const Skills = () => {
     return (
         <section id='skills'>
             <span className="skillTitle">Who We are?</span>
-            <span className="skillDesc">We are InnoAl Tech Solution, a technology company specializing in web development, mobile app development, and database development. Our mission is to help our clients enhance their success in the digital world by providing them with innovative and scalable solutions. With our experienced team, we leverage the latest technologies and best practices to develop your projects. We are committed to delivering quality service by offering tailored solutions to meet our clients' needs. Feel free to contact us for more information.</span>          
+            <span className="skillDesc">We are InnoAl Tech Solution, a technology company focused on web, mobile app, and database development. Our mission is to support clients in the digital world by offering innovative solutions. Our mission is to support clients in the digital world by offering innovative solutions.</span>   
+            <p>Please feel free to contact us for more! </p>       
             <div className="skillBars">
                 <div className="skillBar">
                     <img src={WebDev} alt="WebDevelopent" className="skillBarImg" />
                     <div className="skillBarText">
                         <h2>Web Developent</h2>
-                        <p>We provide personalized web solutions that meet our customer's needs. By working closely with them, we understand their business goals and preferences, shaping the design and coding processes based on their feedback. Our aim is to strengthen their digital presence by delivering dependable, impressive, and adaptable web solutions.</p>
+                        <p>We provide personalized web solutions that meet our customer's needs. By working closely with them, we understand their business goals and preferences, shaping the design and coding processes based on their feedback.Our aim is to strengthen their digital presence by delivering dependable, impressive, and adaptable web solutions. </p>
                     </div>
                 </div>
                 <div className="skillBar">
