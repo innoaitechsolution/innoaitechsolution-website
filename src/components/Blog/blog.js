5c116ed276bbdd7b28c0e8de078d7f9ebf1ab9bb
Arzu Caner <arzuguneycaner@gmail.com>
2024-05-09 10:37:03 +0100

    Fix the blog

--- src/components/Blog/blog.js
+++ src/components/Blog/blog.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './blog.css';
 import BlogImage from '../../assets/blogimage.jpg';
-import { Link } from 'react-router-dom'
-
 
 const Blog = () => {
   return (
     <section id='blog'>
       <span className="blogTitle">InnoAl Tech Solution: Providing Innovative Solutions in the Digital World</span>
-      <span className="blogDesc">At InnoAl Tech Solution, we believe in the power of digital transformation and contribute to our customers' success by providing innovative solutions. Our mission is to optimize business processes, enhance customer experiences, and be a leader in digital transformation.Read more...</span>            
+      <span className="blogDesc">At InnoAl Tech Solution, we believe in the power of digital transformation and contribute to our customers' success by providing innovative solutions. Our mission is to optimize business processes, enhance customer experiences, and be a leader in digital transformation.</span>            
       <div className="blogBars">
         <div className="blogBar">
           <img src={BlogImage} alt="BlogImage" className="blogBarImg" />
