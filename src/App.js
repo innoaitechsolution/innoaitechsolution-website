2f39e4e6b715b963c20e94a627f80b366591891c
Arzu Caner <arzuguneycaner@gmail.com>
2024-08-11 17:21:51 +0100

    Add privacy policy

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Skills from "./components/Skills/skills";
 import Works from "./components/Works/works";
 import Contact from "./components/Contact/contact";
 import Footer from "./components/Footer/footer";
+import PrivacyPolicy from "./privacypolicy/PrivacyPolicy";
 
 function App() {
     return (
@@ -14,7 +15,7 @@ function App() {
             <Works />           
             <Contact />
             <Footer />
-           
+            <PrivacyPolicy />           
         </div>
     );
 }
