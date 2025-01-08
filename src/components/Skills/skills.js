a0353bf4afe7a2c561f68f14dd086866e7d48987
Arzu Caner <arzuguneycaner@gmail.com>
2025-01-08 23:35:41 +0000

    Enhance HomePage layout and design: - Add equal sizing for service and product cards. - Introduce hover effects with purple shadows for cards. - Improve typography and button design for the Hero section. - Refactor CSS for better alignment and styling consistency.

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -8,13 +8,13 @@ const Skills = () => {
     return (
         <section id='skills'>
             <span className="skillTitle">Who We are?</span>
-            <span className="skillDesc">We are InnoAl Tech Solution, a technology company focused on web, mobile app, and database development. Our mission is to support clients in the digital world by offering innovative solutions. Please feel free to contact us for more!</span>                 
+            <span className="skillDesc">InnoAl Tech Solution offers comprehensive web, mobile, and database development services. With our expertise and passion for innovation, we help businesses thrive in the digital landscape. Contact us to discuss your project!</span>                 
             <div className="skillBars">
                 <div className="skillBar">
                     <img src={WebDev} alt="WebDevelopent" className="skillBarImg" />
                     <div className="skillBarText">
                         <h2>Web Developent</h2>
-                        <p>We provide personalized web solutions that meet our customer's needs. By working closely with them, we understand their business goals and preferences, shaping the design and coding processes based on their feedback. </p>
+                        <p>We provide personalized web solutions that meet our customer's needs. By working closely with them, we understand their business goals and preferences, shaping the design and coding processes based on their feedback.</p>
                     </div>
                 </div>
                 <div className="skillBar">
@@ -31,7 +31,7 @@ const Skills = () => {
                         <p>We offer customized database solutions to meet our client's data management needs. Our goal is to help our clients improve their data management processes and achieve their business objectives through database-based solutions.</p>
                     </div>
                 </div>
-            </div>
+            </div>            
         </section>
     );
 }
