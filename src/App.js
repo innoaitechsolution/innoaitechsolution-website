add95d54dff5bda745c321645a29f3f46bdb6274
Arzu Caner <arzuguneycaner@gmail.com>
2025-01-11 00:04:36 +0000

    Update public files and main App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import Navbar from "./components/Navbar/navbar";
-import Intro from './components/Intro/intro';
-import Skills from "./components/Skills/skills";
-import Works from "./components/Works/works";
-import Contact from "./components/Contact/contact";
-import Footer from "./components/Footer/footer";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage";
+import ServicesPage from "./components/ServicesPage/ServicesPage";
+import BlogPage from "./components/Blog/BlogPage";
+import BlogDetailPage from "./components/Blog/BlogDetailPage";
+import ContactPage from "./components/Contact/ContactPage";
+import ProductsPage from "./components/Products/ProductsPage";
 import PrivacyPolicy from "./privacypolicy/PrivacyPolicy";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
-function App() {
-    return (
-        <div className="App">
-            <Navbar />
-            <Intro />
-            <Skills />
-            <Works />           
-            <Contact />
-            <Footer />
-            <PrivacyPolicy />           
-        </div>
-    );
-}
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />      
+        <Route path="/services" element={<ServicesPage />} />       
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/blog/:id" element={<BlogDetailPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
 
 export default App;
