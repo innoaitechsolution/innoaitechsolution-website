79f1b4f3751ba72550892f36cb38ea4c571f4922
Arzu Caner <arzuguneycaner@gmail.com>
2025-01-16 21:48:16 +0000

    Add responsive hamburger menu for Navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
-import logo from "../../assets/logo.png"; 
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo">
@@ -11,12 +17,25 @@ const Navbar = () => {
           <img src={logo} alt="InnoAl Tech Solution Logo" />
         </Link>
       </div>
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/products">Products</Link></li>
-        <li><Link to="/blog">Blog</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+      <div className="hamburger" onClick={toggleMenu}>
+        ☰
+      </div>
+      <ul className={`nav-links ${isOpen ? "open" : ""}`}>
+        <li>
+          <Link to="/" onClick={() => setIsOpen(false)}>Home</Link>
+        </li>
+        <li>
+          <Link to="/services" onClick={() => setIsOpen(false)}>Services</Link>
+        </li>
+        <li>
+          <Link to="/products" onClick={() => setIsOpen(false)}>Products</Link>
+        </li>
+        <li>
+          <Link to="/blog" onClick={() => setIsOpen(false)}>Blog</Link>
+        </li>
+        <li>
+          <Link to="/contact" onClick={() => setIsOpen(false)}>Contact</Link>
+        </li>
       </ul>
     </nav>
   );
