508cfe7dd8c012c99fa1f469ae6cd6d21782e06e
Arzu Caner <arzuguneycaner@gmail.com>
2025-01-28 23:17:10 +0000

    feat: Create AllProductsPage with responsive design

---
+++ src/components/Products/AllProductsPage.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import products from "../../data/products.json"; 
+import "./AllProductsPage.css";
+
+const AllProductsPage = () => {
+  return (
+    <div className="all-products-page">
+      <h1>All Products</h1>
+      <p>Explore our range of products below. Click on a product to learn more or make a purchase.</p>
+      <ul className="product-list">
+        {products.map((product) => (
+          <li key={product.id} className="product-item">
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <a
+              href={product.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="product-link"
+            >
+              View Product
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default AllProductsPage;
